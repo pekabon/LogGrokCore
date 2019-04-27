f0437d66709978ddfee3e9eb3aca28fbf303e3b0
pekabon <skeevee@gmail.com>
2019-04-28 02:06:26 +0300

    copy full path, open containing folder

--- LogGrokCore/DocumentViewModel.cs
+++ LogGrokCore/DocumentViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using ReactiveUI;
+using System;
+using System.Diagnostics;
 using System.IO;
+using System.Reactive;
 using System.Windows.Input;
 
 namespace LogGrokCore
@@ -13,12 +16,24 @@ namespace LogGrokCore
             Title = Path.GetFileName(document.FilePath);
             Text = document.Content;
             CopyPathToClipboardCommand = ReactiveCommand.Create(() => TextCopy.Clipboard.SetText(_document.FilePath));
+            OpenContainingFolderCommand = ReactiveCommand.Create(OpenContainingFolder);
         }
 
         public ICommand CopyPathToClipboardCommand { get; }
-
+        public ICommand OpenContainingFolderCommand { get; }
         public string Title { get; }
-
+        
         public string Text { get; }
+
+        private void OpenContainingFolder()
+        {
+            var filePath = _document.FilePath;
+
+            var cmdLine = File.Exists(filePath) 
+                ? $"/select, {filePath}" 
+                : $"/select, {Directory.GetParent(filePath).FullName}";
+
+            _ = Process.Start("explorer.exe", cmdLine);
+        }
     }
 }
