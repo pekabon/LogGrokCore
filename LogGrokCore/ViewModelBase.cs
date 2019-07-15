94c30a90e4795189d62bb8891989768f18e5166f
pekabon <skeevee@gmail.com>
2019-07-16 00:13:33 +0300

    bugfix

---
+++ LogGrokCore/ViewModelBase.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace LogGrokCore
+{
+    internal abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public virtual event PropertyChangedEventHandler PropertyChanged;
+
+        protected void InvokePropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void SetAndRaiseIfChanged<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (Equals(field, value)) return;
+            field = value;
+            InvokePropertyChanged(propertyName);
+        }
+    }
+}
