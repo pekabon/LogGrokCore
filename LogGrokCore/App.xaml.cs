107874ecbdad101f6f13e518fe3ed81530b7fec7
pekabon <skeevee@gmail.com>
2021-02-08 17:21:22 +0300

    configuration

--- LogGrokCore/App.xaml.cs
+++ LogGrokCore/App.xaml.cs
@@ -1,14 +1,27 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.Drawing;
 using System.Globalization;
 using System.Text;
 using DryIoc;
 using LogGrokCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Primitives;
 using Splat.DryIoc;
 
 namespace LogGrokCore
 {
+    public class ColorSettings
+    {
+        public object[]? Rules { get; set; }
+    }
+
     public partial class App
     {
+        private ColorSettings _olorSettings = new ColorSettings();
+        private IConfigurationRoot _configuration;
+
         public App()
         {
 #if DEBUG
@@ -21,12 +34,27 @@ namespace LogGrokCore
             LoggerRegistrationHelper.Register(container);
             container.UseDryIocDependencyResolver();
             RegisterDependencies(container);
+            
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true);
+            
+            _configuration = builder.Build();
+            
+            _configuration.GetSection("ColorSettings").Bind(_olorSettings);
+
+            ChangeToken.OnChange(() => _configuration.GetReloadToken(), () =>
+            {
+                //_configuration.GetSection("ColorSettings").Bind(_olorSettings);
+                Console.Write(_olorSettings.Rules?.Length);
+            });
 
             Trace.TraceInformation("Initialization complete.");
         }
 
         private static void RegisterDependencies(IRegistrator container)
         {
+            
+            
             container.Register<MainWindowViewModel>();
         }
     }
