68103f307f4a3787037591690893e385e74fa81d
pekabon <skeevee@gmail.com>
2021-03-24 01:32:57 +0300

    layout

--- LogGrokCore/MainWindow.xaml.cs
+++ LogGrokCore/MainWindow.xaml.cs
@@ -1,5 +1,12 @@
-﻿using MahApps.Metro.Controls;
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+using MahApps.Metro.Controls;
 using Splat;
+using Xceed.Wpf.AvalonDock.Layout;
+using Xceed.Wpf.AvalonDock.Layout.Serialization;
 
 namespace LogGrokCore
 {
@@ -7,8 +14,38 @@ namespace LogGrokCore
     {
         public MainWindow()
         {
-            DataContext = Locator.Current.GetService<MainWindowViewModel>(); ;
+            Closing += SaveLayout;
+            Loaded += LoadLayoout;
+
+            DataContext = Locator.Current.GetService<MainWindowViewModel>();
             InitializeComponent();
+        }
+
+        private void LoadLayoout(object sender, RoutedEventArgs e)
+        {
+            if (!File.Exists(GetSettingsFileName())) return;
+            var layoutSerializer = new XmlLayoutSerializer(DockingManager);
+            using var reader = new StreamReader(GetSettingsFileName());
+            layoutSerializer.LayoutSerializationCallback += (o, args) =>
+            {
+                if (args.Model is LayoutDocument)
+                    args.Cancel = true;
+            };
+            layoutSerializer.Deserialize(reader);
+        }
+
+        private void SaveLayout(object sender, CancelEventArgs e)
+        {
+            var serializer = new XmlLayoutSerializer(DockingManager);
+            using var writer = new StreamWriter(GetSettingsFileName());
+            
+            serializer.Serialize(writer);
+        }
+
+        private string GetSettingsFileName()
+        {
+            return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                Process.GetCurrentProcess().ProcessName);
         }
     }
 }
