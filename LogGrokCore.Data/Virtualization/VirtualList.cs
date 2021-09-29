7cf22fa2712da51d9625306764f261ba15798183
pekabon <skeevee@gmail.com>
2021-09-30 00:54:06 +0300

    minor perf optimization

--- LogGrokCore.Data/Virtualization/VirtualList.cs
+++ LogGrokCore.Data/Virtualization/VirtualList.cs
@@ -85,7 +85,7 @@ namespace LogGrokCore.Data.Virtualization
 
         public void CopyTo(Array array, int index) => throw new NotSupportedException();
 
-        private (int indexOnPage, IList<T> page) QueryPage(int index) 
+        private (int indexOnPage, T[] page) QueryPage(int index) 
         {
             var pageIndex = index / PageSize;
             var pageStart = pageIndex * PageSize;
@@ -93,22 +93,22 @@ namespace LogGrokCore.Data.Virtualization
             if (_pageCache.TryGetValue(pageIndex, out var pageWithGeneration))
             {
                 var (page, _) = pageWithGeneration;
-                if (index - pageStart < page.Count) 
+                if (index - pageStart < page.Length) 
                     return (index - pageStart, page);
 
-                var count = Math.Min(PageSize, Count - pageStart) - page.Count;
-                FetchAndConvert(pageStart + page.Count, count, page);
+                var count = Math.Min(PageSize, Count - pageStart) - page.Length;
+                FetchAndConvert(pageStart + page.Length, count, page);
                 return (index - pageStart, page);
             }
 
-            var tempPage = new List<T>(PageSize);
+            var tempPage = ArrayPool<T>.Shared.Rent(PageSize);
 
             FetchAndConvert(pageStart, Math.Min(PageSize, Count - pageStart), tempPage);
             _pageCache[pageIndex] = (tempPage, ++ _pageCounter);
             CleanupCache();
             return (index - pageStart, tempPage);
             
-            void FetchAndConvert(int start, int count, IList<T> pageToAdd)
+            void FetchAndConvert(int start, int count, T[] pageToAdd)
             {
                 var arrayPool = ArrayPool<TSource>.Shared;
                 var newLines = arrayPool.Rent(count);
@@ -119,7 +119,7 @@ namespace LogGrokCore.Data.Virtualization
                     {
                         var lineIndex = idx;
                         var sourceString = newLines[lineIndex];
-                        pageToAdd.Add(_converter(sourceString));
+                        pageToAdd[idx] = _converter(sourceString); 
                     }
                 }
                 finally
@@ -134,14 +134,16 @@ namespace LogGrokCore.Data.Virtualization
         	while (_pageCache.Count > MaxCacheSize)
         	{
                 var oldestKey = _pageCache.OrderBy(pair => pair.Value.Item2).First().Key;
+                var (pageToReturn, _) = _pageCache[oldestKey];
                 _ = _pageCache.Remove(oldestKey);
+                ArrayPool<T>.Shared.Return(pageToReturn);
             }
         }
 
         private const int PageSize = 128;
         private const int MaxCacheSize = 10;
         private readonly Func<TSource, T> _converter;
-        private readonly Dictionary<int, (IList<T>, int)> _pageCache = new();
+        private readonly Dictionary<int, (T[], int)> _pageCache = new();
         private int _pageCounter;
     }
 }
