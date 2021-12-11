5de76407c56b163ebfdf841cdd8b3ace43a2fcd0
pekabon <skeevee@gmail.com>
2021-12-11 22:37:37 +0300

    fix for singleFile deploy

--- LogGrokCore/PathHelpers.cs
+++ LogGrokCore/PathHelpers.cs
@@ -1,16 +1,10 @@
-﻿using System.IO;
-using System.Reflection;
+﻿using System;
+using System.IO;
 
 namespace LogGrokCore
 {
     public static class PathHelpers
     {
-        public static string GetLocalFilePath(string fileName)
-        {
-            var assemblyPath = Assembly.GetCallingAssembly().Location;
-            var directory = Path.GetDirectoryName(assemblyPath);
-            
-            return directory == null ? fileName : Path.Combine(directory, fileName);
-        }
+        public static string GetLocalFilePath(string fileName) => Path.Combine(AppContext.BaseDirectory, fileName);
     }
 }
