5156455a7e295ba3ab79625deb1e4973bf7b704f
pekabon <skeevee@gmail.com>
2021-12-22 00:53:36 +0300

    bugfix & minor perf optimization

--- LogGrokCore/LogMetaInformationProvider.cs
+++ LogGrokCore/LogMetaInformationProvider.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -8,7 +10,8 @@ namespace LogGrokCore
 {
     internal static class LogMetaInformationProvider
     {
-        private const int ProbeSize = 256;
+        private const int ProbeSizeLines = 256;
+        private const int ProbeSizeBytes = 1024 * 1024;
         private const double DetectThreshold = 0.05;
 
         public static LogMetaInformation GetLogMetaInformation(string fileName,
@@ -17,8 +20,9 @@ namespace LogGrokCore
             var metas = logFormats
                 .Select(m => new LogMetaInformation(m));   
 
+            Dictionary<(string fileName, byte mask), IReadOnlyList<string>> cache = new();
             var bestFitter = metas
-                .Select(meta => (meta, ratio: GetRatio(meta, fileName)))
+                .Select(meta => (meta, ratio: GetRatio(meta, fileName, cache)))
                 .OrderByDescending(m => m.ratio)
                 .Where(m => m.ratio > DetectThreshold)
                 .Select(m => m.meta)
@@ -27,27 +31,38 @@ namespace LogGrokCore
             return bestFitter ?? LogMetaInformation.CreateTextFileMetaInformation();                
         }
 
-        private static double GetRatio(LogMetaInformation logMetaInformation, string fileName)
+        private static double GetRatio(LogMetaInformation logMetaInformation, string fileName, 
+            Dictionary<(string fileName, byte mask), IReadOnlyList<string>> cache)
         {
             var regex = new Regex(logMetaInformation.LineRegex);
 
-            IEnumerable<string> ReadLines()
+            IEnumerable<string> ReadLines(string file, byte xorMask)
             {
-                var logFile = new LogFile(fileName, logMetaInformation.XorMask);
+                var logFile = new LogFile(file, xorMask);
                 using var fileStream =  logFile.OpenForSequentialRead();
-                using var streamReader = new StreamReader(fileStream);
+                var buffer = ArrayPool<byte>.Shared.Rent(ProbeSizeBytes);
+                var bytesRead = fileStream.Read(buffer.AsSpan());
+                using var memoryStream = new MemoryStream(buffer, 0, bytesRead);
+                using var streamReader = new StreamReader(memoryStream);
                 while (!streamReader.EndOfStream)
                 {
                     yield return streamReader.ReadLine()!;
                 }
+                ArrayPool<byte>.Shared.Return(buffer);
             }
 
-            var lines = ReadLines()
-                .Take(ProbeSize)
+            var fileAndMask = (fileName, logMetaInformation.XorMask);
+            if (!cache.TryGetValue(fileAndMask, out var sourceLines))
+            {
+                sourceLines = ReadLines(fileAndMask.fileName, fileAndMask.XorMask).Take(ProbeSizeLines).ToList();
+                cache.Add(fileAndMask, sourceLines);
+            }
+
+            var lineGroups = sourceLines
                 .GroupBy(line => regex.IsMatch(line)).ToList();
 
-            var matched = lines.FirstOrDefault(g => g.Key)?.Count() ?? 0;
-            var unmatched = lines.FirstOrDefault(g => !g.Key)?.Count() ?? 0;
+            var matched = lineGroups.FirstOrDefault(g => g.Key)?.Count() ?? 0;
+            var unmatched = lineGroups.FirstOrDefault(g => !g.Key)?.Count() ?? 0;
 
             if (matched + unmatched == 0) return 0;
             return (double) matched / (matched + unmatched);
