4573d285356133f855d1942e69acbc0785ab08e3
pekabon <skeevee@gmail.com>
2022-03-23 00:35:41 +0300

    fix Base64Decode transform

--- LogGrokCore.Data/TransformationPerformer.cs
+++ LogGrokCore.Data/TransformationPerformer.cs
@@ -95,7 +95,7 @@ namespace LogGrokCore.Data
 
         private static string Base64Decode(string input)
         {
-            var buffer = Encoding.UTF8.GetBytes(input);
+            var buffer = Encoding.UTF8.GetBytes(input.Trim('"', ' ', '\t', '\r', '\n'));
             Base64.DecodeFromUtf8InPlace(buffer, out var bytesWritten);
             return Encoding.UTF8.GetString(buffer, 0, bytesWritten);
         }
